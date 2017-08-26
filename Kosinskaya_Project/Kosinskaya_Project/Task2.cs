be53fd905ad3cc020451ccd4c20576d91a80ed8b
vita kosinskaya <vitakosinskaya@gmail.com>
2017-08-27 02:16:46 +0500

    Выполнено задание №3

---
+++ Kosinskaya_Project/Kosinskaya_Project/Task2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class MySeconfTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new InternetExplorerDriver(@"C:\Tools\");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            Thread.Sleep(1000);
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+            driver.Close();
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
